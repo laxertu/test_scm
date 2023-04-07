519bde934ecc0b23cd70862a3c0e80e9b6321c3d
luca@igor <luca@igor>
2023-04-07 17:19:48 +0200

    hkhkh

--- get_version.py
+++ get_version.py
@@ -8,11 +8,9 @@ tags = [t.name for t in repo.tags]
 v = setuptools_scm.get_version()
 if str(repo.active_branch) == "master":
     v_clean = ".".join(v.split(".")[0:3])
-    if v_clean != v and (v_clean not in tags):
-        tmp = v_clean.split(".")
-        v = f"{tmp[0]}.{tmp[1]}.{int(tmp[2]) + 1}"
-    else:
-        v = ""
+    tmp = v_clean.split(".")
+    v = f"{tmp[0]}.{tmp[1]}.{int(tmp[2]) + 1}"
+
 else:
     v = ""
 

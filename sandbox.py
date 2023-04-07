47660d0a252770a3989ac0c6596979dde3278679
luca@igor <luca@igor>
2023-04-07 13:43:39 +0200

    minor changes

--- sandbox.py
+++ sandbox.py
@@ -3,20 +3,20 @@ import setuptools_scm
 
 repo = Repo()
 
-print("tags:")
+print("Tags:")
 for t in repo.tags:
     print(t.name)
 print("")
 
 v = setuptools_scm.get_version()
-print("on branch", repo.active_branch)
-print("raw version", v)
+print("On branch", repo.active_branch)
+print("Raw version", v)
 if str(repo.active_branch) == "master":
     v = ".".join(v.split(".")[0:3])
 else:
     v = v.split("+")[0].replace("dev", "rc")
 
-print("guessed", v)
+print("Guessed", v)
 
 
 

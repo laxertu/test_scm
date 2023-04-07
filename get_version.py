7b8069e4cbb5402ce5ce2dcede7f81446c809469
luca@igor <luca@igor>
2023-04-07 17:07:02 +0200

    ss

--- get_version.py
+++ get_version.py
@@ -7,7 +7,12 @@ tags = [t.name for t in repo.tags]
 
 v = setuptools_scm.get_version()
 if str(repo.active_branch) == "master":
-    v = ".".join(v.split(".")[0:3])
+    v_clean = ".".join(v.split(".")[0:3])
+    if v_clean != v and (v_clean not in tags):
+        tmp = v_clean.split(".")
+        v = f"{tmp[0]}.{tmp[1]}.{int(tmp[2]) + 1}"
+    else:
+        v = ""
 else:
     v = ""
 
--- pipeline_inegration.sh
+++ pipeline_inegration.sh
@@ -4,6 +4,7 @@ VERSION=$(python get_version.py)
 
 if [ "$VERSION" != "" ]; then
 echo "${VERSION}"
+echo "git tag ${VERSION}"
 exec git tag $VERSION
 fi
 

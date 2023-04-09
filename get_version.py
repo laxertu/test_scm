7c67ed53965c14d7e88132a35fe129b7725d1bb7
luca@igor <luca@igor>
2023-04-09 12:45:08 +0200

    first

--- get_version.py
+++ get_version.py
@@ -1,2 +1,10 @@
+from git import Repo
 from setuptools_scm import get_version
-print(get_version())
+
+branch = Repo().active_branch
+if str(branch) == "master":
+    v = get_version()
+    v = ".".join(v.split(".")[0:3])
+    print(".".join(v.split(".")[0:3]))
+else:
+    print("")

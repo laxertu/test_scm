09aef158b9db67bbb874f0ec5b5ac34a43542d2a
luca@igor <luca@igor>
2023-04-09 12:38:43 +0200

    first

--- pipeline_inegration.sh
+++ pipeline_inegration.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-REPOSITORY = $(python get_current_branch.py)
+REPOSITORY=$(python get_current_branch.py)
 VERSION=$(python get_version.py)
 
 if [ "$REPOSITORY" == "master" ]; then
   echo "git tag ${VERSION}"
   git tag $VERSION
-  git push --tags
 fi
 
 echo "${VERSION}"

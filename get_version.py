d4d4f6c1dce32458704735d4ee8836686d8021cf
luca@igor <luca@igor>
2023-04-16 13:55:08 +0200

    first

--- get_version.py
+++ get_version.py
@@ -1,5 +1,5 @@
 from setuptools_scm import get_version
-print(get_version(local_scheme="no-local-version"))
+print(get_version(version_scheme="release-branch-semver", local_scheme="no-local-version"))
 """
 import pkg_resources
 version = pkg_resources.require("test_scm")[0].version

19df9995594c189d12a0d6d0de45ba2f4c729329
luca@igor <luca@igor>
2023-04-19 14:51:55 +0200

    gyuy

--- tag_repo.sh
+++ tag_repo.sh
@@ -6,8 +6,11 @@ repository=$(git rev-parse --abbrev-ref HEAD)
 if [ "$repository" = "master" ]; then
   package_current_version=$(python setup.py --version)
   package_next_version=$(python get_version_to_tag.py ${package_current_version})
-  git tag $package_next_version
-  echo "tagged package version"
-  echo "${package_next_version}"
+
+  if [ "$package_next_version" != "" ]; then
+    git tag $package_next_version
+    echo "tagged package version"
+    echo "${package_next_version}"
+  fi
 fi
 

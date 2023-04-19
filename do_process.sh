5af4bb2b827a04cf91e6de85d1ab95eb8e34cc8b
luca@igor <luca@igor>
2023-04-19 14:41:19 +0200

    first

--- do_process.sh
+++ do_process.sh
@@ -2,3 +2,7 @@
 
 ./tag_repo.sh
 python setup.py  bdist_wheel
+
+CURRENT_VERSION=$(python setup.py --version)
+echo "installed version"
+echo "${CURRENT_VERSION}"
--- tag_repo.sh
+++ tag_repo.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-REPOSITORY=$(git rev-parse --abbrev-ref HEAD)
+repository=$(git rev-parse --abbrev-ref HEAD)
 # git describe --tags --abbrev=0
 
-if [ "$REPOSITORY" = "master" ]; then
-  CURRENT_VERSION = $(python setup.py --version)
-  VERSION=$(python get_version.py ${CURRENT_VERSION})
-  git tag $VERSION
+if [ "$repository" = "master" ]; then
+  package_current_version=$(python setup.py --version)
+  package_next_version=$(python get_version.py ${package_current_version})
+  git tag $package_next_version
   echo "tagged package version"
-  echo "${VERSION}"
+  echo "${package_next_version}"
 fi
 

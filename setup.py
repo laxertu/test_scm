f9ed58baa85909e23606c76ca39400ca827b8c01
luca@igor <luca@igor>
2023-04-20 20:02:29 +0200

    rrr

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ import setuptools_scm.version
 
 def myversion():
     from setuptools_scm.version import guess_next_date_ver
+    from setuptools_scm import get_version
 
     def get_next_version(version: setuptools_scm.version.ScmVersion):
-        return version.format_next_version(guess_next_date_ver)
+        return get_version() if version.branch == "master" else version.format_next_version(guess_next_date_ver)
 
 
     return {

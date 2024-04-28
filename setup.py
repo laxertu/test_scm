ea0342946974b1aa5d02f741df4f72668bef7969
luca@igor <luca@igor>
2024-04-28 09:31:53 +0200

    set fmt to format_next_version

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ def myversion():
         if version.branch == "master" and version.distance is None:
             return get_version()
         else:
+            print(version.distance)
             return version.format_next_version(guess_next_date_ver)
 
 

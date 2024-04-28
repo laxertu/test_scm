9cf06d3f93f8d9d90be85eb1e13058c3a7715c35
luca@igor <luca@igor>
2024-04-28 09:03:00 +0200

    set fmt to format_next_version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def myversion():
     def get_next_version(version: setuptools_scm.version.ScmVersion):
         version.time = datetime.datetime.now()
         # fallback_version="0.0.0"
-
+        print(version.distance)
         if version.branch == "master":
             if version.distance == 0:
                 return get_version()
